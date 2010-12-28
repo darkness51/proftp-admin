e6f3aa2d47778e72adc999fe43f0755837b3a30b
Carlos Aguilar <caguilar@dwdandsolutions.com>
2010-12-27 21:46:33 -0600

    Agregados a la interfaz de administracion los modelos y creada una redirección para el administrador de django.

---
+++ src/ftpadmin/main/admin.py
@@ -0,0 +1,6 @@
+from models import Usuario, Group, Quota_Limits
+from django.contrib import admin
+
+admin.site.register(Usuario)
+admin.site.register(Group)
+admin.site.register(Quota_Limits)
--- src/ftpadmin/main/views.py
+++ src/ftpadmin/main/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import redirect
+
+def index(request):
+    '''
+    Redirecciona hacia el administrador
+    '''
+    return redirect('/admin/', permanent=True)
